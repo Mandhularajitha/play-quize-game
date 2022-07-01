ed57fe18f0a971d551872f167e497535aa604535
Mandhularajitha <rajitha21@navgurukul.org>
2022-07-01 09:44:15 +0530

    added changes

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -8,7 +8,7 @@ const Start = ({ onQuizStart }) => {
       </div>
       <div className="card-content">
         <div className="content">
-          <h1 className='text_conntainer1'>Welcome to My Quize App</h1>
+          {/* <h1 className='text_conntainer1'>Welcome to My Quize App</h1> */}
           <h1 className='text_container'>Start the quiz</h1>
           <p className='text1'>Good luck!</p>
           <button className="button_info" onClick={onQuizStart}>Start</button>
